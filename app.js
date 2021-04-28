d8ed57b6b5c31bbda9ec09a034271a3334cd7d8e
Jose E. Lozano Jr <josee.lozanojr.@MacBook-Pro.local>
2021-04-28 10:24:45 -0500

    added styling to search results

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
-    var $containerDiv = $("<div id='container'></div>");
+    var $containerDiv1 = $("<div id='container1'></div>");
 
-    $containerDiv.appendTo("body");
+    $containerDiv1.appendTo("body");
 
     var $containerDiv2 = $("<div id='container2'></div>");
 
@@ -9,9 +9,13 @@ $( document ).ready(function() {
 
     var $searchButton = $("button");
 
+    // function pressedReturn(event) {
+    //     event.target
+    // }
+
     $searchButton.click(function(){
 
-        $containerDiv.empty();
+        $containerDiv1.empty();
 
         $containerDiv2.empty();
 
@@ -22,22 +26,22 @@ $( document ).ready(function() {
             console.log(searchResults);
 
             let $infoDiv = $("<div>Click on Name for More information.</div>");
-            $infoDiv.appendTo($containerDiv);
+            $infoDiv.appendTo($containerDiv1);
 
             for (let index of searchResults) {
                 let $fullName = index.name;
-                let $div = $(`<div>${$fullName}</div>`);
+                let $resultDiv = $(`<div class='resultDiv'>${$fullName}</div>`);
                 console.log($fullName)
-                $div.appendTo($containerDiv);
-                $div.click(function() {makeDiv(index)});
+                $resultDiv.appendTo($containerDiv1);
+                $resultDiv.on('click', function() {makeDiv(index)});
             };
 
             function makeDiv(index) {
                 console.log(index)
                 $containerDiv2.empty();
                 for (let key in index) {
-                let $div2 = $(`<div>${key}: ${index[key]}</div>`);
-                $div2.appendTo($containerDiv2);
+                let $infoCard = $(`<div>${key}: ${index[key]}</div>`);
+                $infoCard.appendTo($containerDiv2);
                 };
             }
 
